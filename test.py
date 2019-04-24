8c205d187a716634ae688e3a7e35c2778a2d7a96
Tim Mulej <tim.mulej@student.fmf.uni-lj.si>
2019-04-24 15:09:36 +0200

    Still trying to get a merge conflict

--- test.py
+++ test.py
@@ -1,12 +1,20 @@
 # Napiši program, ki izpiše prvih 200 praštevil
 
-def je_prastevilo (n):
-    for i in range (2, n):
+def je_prmjsfbljhgastevilo (n):
+    for i in range (2, n):sdgsgs
         if n % i == 0:
             return False
     return True
 
 for x in range (1, 201):
-    if je_prastevilo(x):
         print(x)
+
+
+
+
+
+        fhdshxgdx
+        gfhfdghdc
+
+        gffffhd
 
